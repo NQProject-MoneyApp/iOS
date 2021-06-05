3def1753340b8787113fc0079468d176e0c6ab2d
Szymon Gesicki <szym.gesicki@gmail.com>
2021-06-05 18:15:43 +0200

    create empty project

---
+++ MoneyApp/MoneyApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MoneyApp
+//
+//  Created by Szymon Gęsicki on 05/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+

d41695934bdb615853d26519759f6c68bb15aafa
Szymon Gesicki <szym.gesicki@gmail.com>
2021-09-11 13:29:56 +0200

    added refresh on expenses list, information about no expenses, some animations

--- MoneyApp/MoneyApp/UI/Views/AllExpenses/AllExpensesViewController.swift
+++ MoneyApp/MoneyApp/UI/Views/AllExpenses/AllExpensesViewController.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-class AllExpensesViewController: UIViewController {
+class AllExpensesViewController: UIViewController, ScrollViewRefreshDelegate {
     
     static func loadFromStoryboard() -> AllExpensesViewController? {
         let storyboard = UIStoryboard(name: "AllExpensesView", bundle: nil)
@@ -18,6 +18,21 @@ class AllExpensesViewController: UIViewController {
     private let scrollView = ScrollView()
     private let service = AllExpensesService()
     var group: Group?
+    
+    func didRefreshList(refreshCompletion: @escaping () -> Void) {
+        guard let group = group else { return }
+        
+        let now = Date().timeIntervalSince1970
+        
+        service.fetchExpenses(groupID: group.id, completion: { expenses in
+            // quick UI fix
+            DispatchQueue.main.asyncAfter(deadline: .now() + (1 - (Date().timeIntervalSince1970 - now))) {
+                self.scrollView.clearComponents()
+                self.createContent(expenses: expenses)
+                refreshCompletion()
+            }
+        })
+    }
 
     override func viewDidLoad() {
         view.backgroundColor = UIColor.brand.blackBackground
@@ -26,15 +41,38 @@ class AllExpensesViewController: UIViewController {
         setupScrollView()
         
         guard let group = group else { return }
-        service.fetchExpenses(groupID: group.id, completion: { result in
+
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.color = UIColor.brand.yellow
+        activityIndicator.startAnimating()
+        scrollView.setSingleContent(content: activityIndicator)
+        
+        service.fetchExpenses(groupID: group.id, completion: { expenses in
+            self.scrollView.clearComponents()
+            self.createContent(expenses: expenses)
+            self.scrollView.alpha = 0
+            self.scrollView.fadeIn(0.5)
+        })
+    }
+        
+    private func createContent(expenses: [Expense]) {
+        
+        if expenses.isEmpty {
             
-            for (idx, expense) in result.enumerated() {
-                print("expense \(expense.name) \(idx == result.count - 1)")
+            let text = UILabel()
+            text.text = "No expenses yet"
+            text.textColor = UIColor.brand.yellow
+            text.font = UIFont.systemFont(ofSize: 32)
+            text.textAlignment = .center
+            scrollView.setSingleContent(content: text)
+            
+        } else {
+            for (idx, expense) in expenses.enumerated() {
                 let expenseComponent = ExpenseComponent()
                 expenseComponent.create(expense: expense)
-                self.scrollView.append(component: expenseComponent, last: idx == result.count - 1)
+                scrollView.append(component: expenseComponent, last: idx == expenses.count - 1)
             }
-        })
+        }
     }
     
     private func setupScrollView() {
@@ -43,5 +81,7 @@ class AllExpensesViewController: UIViewController {
         scrollView.snp.makeConstraints { make in
             make.edges.equalTo(view)
         }
+        
+        scrollView.setRefreshDelegate(delegate: self)
     }
 }
